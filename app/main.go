331ea9af1ab9f07a6bdf12f57328ee31b28075e6
samuel-eric <samueltan086@gmail.com>
2026-01-05 21:26:38 +0700

    add type command

--- app/main.go
+++ app/main.go
@@ -18,16 +18,25 @@ func main() {
 		}
 
 		input = input[:len(input)-1]
-
 		cmd, arg := strings.Split(input, " ")[0], strings.Split(input, " ")[1:]
+		argStr := strings.Join(arg, " ")
+
 		switch cmd {
 		case "exit":
 			os.Exit(0)
 		case "echo":
-			fmt.Println(strings.Join(arg, " "))
+			fmt.Println(argStr)
+		case "type":
+			validCmds := map[string]bool{"exit": true, "echo": true, "type": true}
+			if _, ok := validCmds[argStr]; ok {
+				fmt.Printf("%s is a shell builtin\n", argStr)
+			} else {
+				fmt.Printf("%s: not found\n", argStr)
+			}
 		default:
 			fmt.Printf("%s: command not found\n", cmd)
 		}
+
 		fmt.Print("$ ")
 	}
 }
