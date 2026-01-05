547d54cacc93a5591bc5e42206f02416f0677871
samuel-eric <samueltan086@gmail.com>
2026-01-05 21:19:52 +0700

    add echo command

--- app/main.go
+++ app/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var cmd string
 	fmt.Print("$ ")
 
 	for {
-		fmt.Scanln(&cmd)
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("error reading command")
+			os.Exit(1)
+		}
+
+		input = input[:len(input)-1]
+
+		cmd, arg := strings.Split(input, " ")[0], strings.Split(input, " ")[1:]
 		switch cmd {
 		case "exit":
 			os.Exit(0)
+		case "echo":
+			fmt.Println(strings.Join(arg, " "))
 		default:
 			fmt.Printf("%s: command not found\n", cmd)
 		}
