db70e966aac24a42d69c50db965f6f41c9707a3f
samuel-eric <samueltan086@gmail.com>
2026-01-05 21:09:05 +0700

    add REPL

--- app/main.go
+++ app/main.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
+	var cmd string
 	fmt.Print("$ ")
 
-	var cmd string
-	fmt.Scanln(&cmd)
-	fmt.Printf("%s: command not found\n", cmd)
+	for {
+		fmt.Scanln(&cmd)
+		fmt.Printf("%s: command not found\n", cmd)
+		fmt.Print("$ ")
+	}
 }
