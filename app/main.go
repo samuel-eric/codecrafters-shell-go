19b8f949423e342fc88b5a2911c6e3d433cc9a3c
samuel-eric <samueltan086@gmail.com>
2026-01-05 21:04:03 +0700

    handle invalid commands

--- app/main.go
+++ app/main.go
@@ -10,4 +10,8 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	var cmd string
+	fmt.Scanln(&cmd)
+	fmt.Printf("%s: command not found\n", cmd)
 }
